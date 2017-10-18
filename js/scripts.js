77c56bf9d186a9e8c75c715b88bd781b3dcaecfb
Colin Worf and Michael Pinaud <michael@>
2017-10-18 11:02:31 -0700

    add jquery function for click event, will display the name.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-var firstName = $("input#first-name").val()
-var lastName = $("input#last-name").val()
-$("")
+$(function(){
+  $("#form").submit(function(event){
+    var name = $("input#first-name").val() + " " + $("input#last-name").val();
+    event.preventDefault();
+    $("#name").text(name);
+    $(".card-footer").show();
+  });
+});
